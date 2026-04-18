f52345993ad34ad2014e4e98e6953b56cc346924
banana3216-2025 <banana.3216.2025@gmail.com>
2026-04-18 03:10:01 +0000

    remove helium

--- programs/helium.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  environment.systemPackages = [
-    (import (builtins.fetchTarball "https://github.com") {}).defaultPackage.${pkgs.system}
-    pkgs.nur.repos.Ev357.helium
-  ];
-}
